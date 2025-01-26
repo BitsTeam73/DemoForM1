1d19faae90a0877fc7d5f70def226ebd0106a5eb
Sai Vennala Sunku <sai.vennala19@gmail.com>
2025-01-26 12:40:10 +0530

    Updated test model

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,9 @@
+import sys
 import os
-from src.train import train_model
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+from train import train_model
 
 def test_training_script():
-    # Run the training function
     train_model()
-    
-    # Check if the model file is created
     assert os.path.exists("model.pkl")
