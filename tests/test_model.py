103076b4f512aa7d3dc1caa8ae0226214eb3961b
Sai Vennala Sunku <sai.vennala19@gmail.com>
2025-01-27 12:18:59 +0530

    Updated logic for tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,8 @@
-from src.train import train_model
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+from train import train_model
 
 def test_training_script():
     train_model()
