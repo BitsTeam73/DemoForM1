8b861e78e39e5b333e35d8ea5dcd4da483e9a393
Sai Vennala Sunku <sai.vennala19@gmail.com>
2025-01-27 11:23:53 +0530

    Updated comments

--- src/train.py
+++ src/train.py
@@ -5,7 +5,7 @@ from sklearn.metrics import accuracy_score
 
 
 def train_model():
-    """Train a logistic regression model using the Iris dataset and evaluate its accuracy."""
+    """Train a logistic regression model using the Iris dataset."""
     # Load dataset
     data = load_iris()
     X_train, X_test, y_train, y_test = train_test_split(
