33e31f39af57f87b530e152f5acfa033492cd199
Sai Vennala Sunku <sai.vennala19@gmail.com>
2025-01-27 11:05:16 +0530

    removed os import

--- src/train.py
+++ src/train.py
@@ -3,7 +3,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import joblib
-import os
 
 
 def train_model():
